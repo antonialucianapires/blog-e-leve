820d38456d1e79ad97f2470742494876626b498a
antonialucianapires <antonialucianapires@gmail.com>
2020-02-27 23:58:25 -0300

    Atualizacao de configuracoes do servidor e criacao das pastas public e views

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const  bodyParser = require('body-parser');
+
+//view engine
+app.set('view engine', 'ejs');
+
+//static
+app.use(express.static('public'));
+
+//body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('teste')
+    res.render('index')
 });
 
 app.listen(3000, (req, res) => {
