4d16588f7b44c8d7195effce2b815ec4c8000e67
antonialucianapires <antonialucianapires@gmail.com>
2020-02-29 00:40:42 -0300

    crud-categoria: criacao da rota e metodo para atualizar uma categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -8,12 +8,11 @@ router.get('/categories', (req, res) => {
     res.send('rota de categoria')
 });
 
-//Create
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new');
 });
 
-//save category
+//Create
 router.post('/categories/save', (req, res) => {
     var title = req.body.title;
     if( title != undefined) {
@@ -58,6 +57,39 @@ router.post('/categories/delete', (req, res) => {
     }else { 
         res.redirect('/admin/categories')
     }
+});
+
+//route edit
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)) {
+        res.redirect('/admin/categories');
+    }
+
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+            res.render('admin/categories/edit', {category: category});
+        } else {
+            res.redirect('/admin/categories');
+        }
+    }).catch(erro => {
+        res.redirect('/admin/categories');
+    })
+});
+
+//update
+router.post('/categories/update', (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect('/admin/categories');
+    });
 });
 
 module.exports = router;
