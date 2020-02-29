0cfcdbba7050706a11a8c763d37c45cdb3e6d54c
antonialucianapires <antonialucianapires@gmail.com>
2020-02-28 23:50:48 -0300

    crud-categoria: rota para criar e salvar uma nova categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+const Category = require('./Category');
 
 //todas as categorias
 router.get('/categories', (req, res) => {
     res.send('rota de categoria')
 });
 
-//nova categoria
+//new category
 router.get('/admin/categories/new', (req, res) => {
-    res.send('rota da nova categoria')
+    res.render('admin/categories/new');
+});
+
+//save category
+router.post('/categories/save', (req, res) => {
+    var title = req.body.title;
+    if( title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect('/');
+        })
+    } else {
+        res.redirect('/admin/categories/new');
+    }
 });
 
 module.exports = router;
